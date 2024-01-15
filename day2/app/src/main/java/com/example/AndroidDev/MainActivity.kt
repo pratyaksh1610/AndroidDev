df4b6db312c9b88fc5cdc36dee57e031755ad5d0
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2024-01-15 13:45:26 +0530

    :white_check_mark: day2

---
+++ day2/app/src/main/java/com/example/AndroidDev/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.AndroidDev
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.demo2024.R
+import com.example.demo2024.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnLight.setOnClickListener {
+            binding.ll.setBackgroundResource(R.color.yellow)
+        }
+        binding.btnDark.setOnClickListener {
+            binding.ll.setBackgroundResource(R.color.black)
+        }
+    }
+}
